e3bf716166dddaedf4caa8ac93feea9cf2a4f310
Artem Bobylev <artematrr@gmail.com>
2024-11-11 01:36:32 +0400

    feat: LAB4-3 Добавил ранее созданные страницы в проект

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,76 @@
-const path = require('path') // Импортируем модуль "path" для работы с путями файлов
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js', // Точка входа для сборки проекта
+  entry: './src/index.js',
 
-	output: {
-		filename: 'bundle.js', // Имя выходного файла сборки
-		path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
-	},
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.css$/, // Регулярное выражение для обработки файлов с расширением .css
-				use: ['style-loader', 'css-loader'], // Загрузчики, используемые для обработки CSS-файлов
-			},
-		],
-	},
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        include: path.resolve(__dirname, 'src/img'),
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][ext]',
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        include: path.resolve(__dirname, 'src/img/icon'),
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/icon/[name][ext]',
+        },
+      },
+    ],
+  },
 
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-			inject: true,
-			chunks: ['index'],
-			filename: 'index.html',
-		}),
-	],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/about.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'about.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/projects.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'projects.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/tasks.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'tasks.html',
+    }),
+  ],
 
-	devServer: {
-		static: {
-			directory: path.join(__dirname, 'dist'), // Каталог для статики
-		},
-		open: true, // Автоматически открывать браузер
-	},
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'src'),
+      publicPath: '/',
+    },
+    open: true,
+    hot: true,
+    port: 8080,
+  },
 
-	mode: 'development', // Режим сборки
-}
+  mode: 'development',
+};
